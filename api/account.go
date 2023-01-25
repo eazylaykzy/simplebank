eb8047731333d9adbbf40d14c7ba5037a97b2eee
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-01-25 08:53:24 +0100

    feat(): handle DB errors

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	db "github.com/eazylaykzy/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	"net/http"
 )
 
@@ -95,6 +96,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
