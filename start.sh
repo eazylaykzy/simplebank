df0980fe6b9c2d70c432b308314240485b5f4a79
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-04 14:30:29 +0100

    fix(): debug cluster's issue

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "run db migration"
-source /app/app.env
+echo "run db migration - source: $DB_SOURCE"
+. /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
