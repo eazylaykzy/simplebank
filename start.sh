45294e0cfcf2732aed6e9053c857c02e2773d185
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-05 10:23:46 +0100

    feat(): add ingress to route traffic to different services in K8

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "run db migration"
 . /app/app.env
-echo "DB source: $DB_SOURCE"
 #source /app/app.env # this highlights error
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
