ca29b022c6cd3a4b24d425323ee8ec0307efcacd
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-08 22:20:08 +0100

    Feat/grpc gateway (#22)
    * feat(): implement and initiate a gRPC service and run it with evans
    
    * feat(): implement gRPC services
    
    * feat(): implement gRPC gateway to generate both gRPC services and normal RESTful HTTP

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"github.com/eazylaykzy/simplebank/api"
 	db "github.com/eazylaykzy/simplebank/db/sqlc"
 	"github.com/eazylaykzy/simplebank/gapi"
 	"github.com/eazylaykzy/simplebank/pb"
 	"github.com/eazylaykzy/simplebank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
@@ -22,6 +25,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -47,7 +51,48 @@ func runGrpcServer(config util.Config, store db.Store) {
 	}
 }
 
-func runGinServer(config util.Config, store db.Store) {
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server:", err)
+	}
+}
+
+/*func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
@@ -57,4 +102,4 @@ func runGinServer(config util.Config, store db.Store) {
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
-}
+}*/
