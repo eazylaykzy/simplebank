4c196c4685cc81ee093cd1badcb6509bc144dc73
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-10 09:41:50 +0100

    feat(): add all frontend data into the binary for easy deployment to production

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"database/sql"
 	db "github.com/eazylaykzy/simplebank/db/sqlc"
+	_ "github.com/eazylaykzy/simplebank/doc/statik"
 	"github.com/eazylaykzy/simplebank/gapi"
 	"github.com/eazylaykzy/simplebank/pb"
 	"github.com/eazylaykzy/simplebank/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -79,8 +81,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
