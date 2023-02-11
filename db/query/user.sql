c7f19665357c328911429fe676025e02e0b1e88b
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-11 21:17:49 +0100

    feat(): optional update to gRPC fields

--- db/query/user.sql
+++ db/query/user.sql
@@ -14,8 +14,9 @@ LIMIT 1;
 
 -- name: UpdateUser :one
 UPDATE users
-SET hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
-    full_name       = COALESCE(sqlc.narg(full_name), full_name),
-    email           = COALESCE(sqlc.narg(email), email)
+SET hashed_password     = COALESCE(sqlc.narg(hashed_password), hashed_password),
+    password_changed_at = COALESCE(sqlc.narg(password_changed_at), password_changed_at),
+    full_name           = COALESCE(sqlc.narg(full_name), full_name),
+    email               = COALESCE(sqlc.narg(email), email)
 WHERE username = sqlc.arg(username)
 RETURNING *;
