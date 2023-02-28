f22a073466b3d99ce22f6b42dda64b36eec9bc3a
Adeleke Adeniji <adelekeadenijio@gmail.com>
2023-02-28 21:01:08 +0100

    Feat/background worker (#32)
    * feat(): add asynq for distributed worker
    
    * feat(): implement asynq for distributed worker

--- gapi/server.go
+++ gapi/server.go
@@ -2,6 +2,7 @@ package gapi
 
 import (
 	"fmt"
+	"github.com/eazylaykzy/simplebank/worker"
 
 	db "github.com/eazylaykzy/simplebank/db/sqlc"
 	"github.com/eazylaykzy/simplebank/pb"
@@ -12,22 +13,24 @@ import (
 // Server serves gRPC requests for our banking service.
 type Server struct {
 	pb.UnimplementedSimpleBankServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates a new gRPC server.
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
